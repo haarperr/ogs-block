085e0b5ddb6675a85c9e6b352dcc29a37b0a2970
Cauê <45627677+CaueDev@users.noreply.github.com>
2022-02-20 03:15:04 -0300

    fix(gameplay): disable weapon drops

--- resources/[base]/caue-gameplay/client/cl_weapondrops.lua
+++ resources/[base]/caue-gameplay/client/cl_weapondrops.lua
@@ -1,13 +1,15 @@
 Citizen.CreateThread(function()
-	local pedPool = GetGamePool("CPed")
+	while true do
+        local pedPool = GetGamePool("CPed")
 
-    for k,v in pairs(pedPool) do
-		SetPedDropsWeaponsWhenDead(v, false)
+        for k, v in pairs(pedPool) do
+            SetPedDropsWeaponsWhenDead(v, false)
 
-        if DecorGetInt(v, "NPC_ID") == 0 and not DecorExistOn(v, "ScriptedPed") and Config.BlacklistedPeds[GetEntityModel(v)] then
-			Sync.DeleteEntity(v)
-		end
-	end
+            if Config.BlacklistedPeds[GetEntityModel(v)] and DecorGetInt(v, "NPC_ID") == 0 and not DecorExistOn(v, "ScriptedPed") then
+                Sync.DeleteEntity(v)
+            end
+	    end
 
-	Citizen.Wait(500)
+	    Citizen.Wait(500)
+    end
 end)
