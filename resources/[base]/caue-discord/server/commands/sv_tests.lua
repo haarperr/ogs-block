a7da89b6d7823f1b597211ccc7a194a4de6e01ec
Cauê <45627677+CaueDev@users.noreply.github.com>
2022-03-01 00:40:18 -0300

    update(discord): players function

--- resources/[base]/caue-discord/server/commands/sv_tests.lua
+++
@@ -1,6 +0,0 @@
-Commands["playercount"] = {
-    ["DISCORD_IDS"] = { "228659194771800065" },
-    ["FUNCTION"] = function(params, author)
-        sendToDiscord("Player Counts", "Current players in server : " .. GetNumPlayerIndices(), 4620980)
-    end,
-}
